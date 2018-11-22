a65aa003dbc6c8395d37b670e408612c674f158c
kk <kiky.li@rtbasia.com>
2018-11-22 13:54:56 +0800

    分解合并服务

--- app/services/blacklist-service.js
+++ app/services/blacklist-service.js
@@ -2,15 +2,11 @@
 
 let queryCC = require('../cc/query')
 let invokeCC = require('../cc/invoke')
-var CONFIG = require('../../config.json')
 var CONFIG_IPFS = require('../../config.json').site.ipfs
-var agent = require('superagent-promise')(require('superagent'), Promise)
-let ADMIN_ADDR = CONFIG.site.adminAddr
 let logger = require('../utils/logger-utils').logger()
 
 let ipfsCliLocal = require('../utils/ipfs-cli')
 let ipfsCliRemote = require('../utils/ipfs-cli-remote').bind(CONFIG_IPFS.host, CONFIG_IPFS.port)
-var BloomFilter = require('bloomfilter').BloomFilter
 
 /**
  * 上传申诉
@@ -66,70 +62,42 @@ async function commitBlacklist (filename, newBlacklist, type, newBlacklistIpfs, 
 }
 
 async function merge (type, latestVersion) {
-  // 获取共识的移除清单
-  let startTime = new Date().getTime()
-  logger.info(`[${type}]:start to download consused rm list of all orgs`)
-  let rmSetOfConsensus = await _getConsensusedRmList(type)
-  let endTime = new Date().getTime()
-  logger.info(`[${type}]:end to download consused rm list of all orgs:${endTime - startTime}ms`)
+  // 查询所有组织的移除列表信息
+  let allRmListInfo = await queryCC('getRemoveList', [type])
 
-  logger.info(`[${type}]:start to download full list`)
   // 获取合并的全量列表
-  let mergedFullList = await _getMergedFullListOfOrgs(type)
-  let endTimeOfDownFull = new Date().getTime()
-  logger.info(`[${type}]:end to download full list:${endTimeOfDownFull - endTime}ms`)
+  let allOrgsFulllists = await queryCC('getAllOrgsList', [type])
 
+  // 提交合并任务
+  submitMergeToJobHistory(type, latestVersion, allRmListInfo, allOrgsFulllists)
+}
+
+/**
+ * // TODO:提交合并任务
+ **/
+async function submitMergeToJobHistory (type, latestVersion, allRmListInfo, allOrgsFulllists) {
   // 剔除媒体ip
+  let publishIps = ''
   if (type === 'ip') {
-    logger.info(`[${type}]:start to filter ips of publishers`)
-    mergedFullList = await filterPublisherIps(mergedFullList)
-    let endOfFilter = new Date().getTime()
-    logger.info(`[${type}]:end to filter ips of publishers:${endOfFilter - endTimeOfDownFull}ms`)
+    publishIps = await queryCC('getPublisherIp', [])
   }
+  console.log('————>', publishIps)
+}
 
-  // 剔除不符合的记录
-  logger.info(`[${type}]:start to consenus`)
-  let startTimeOfConsensus = new Date().getTime()
-  let finnalResult = _getFinnalRecords(mergedFullList, rmSetOfConsensus, type)
-  mergedFullList = finnalResult.tmpMergedFullList
-  let bloomFilter = finnalResult.bloomFilter
-  let endOfConsensus = new Date().getTime()
-  logger.info(`[${type}]:end to consenus:${endOfConsensus - startTimeOfConsensus}ms`)
-  // 将投票集合转换为列表
-  let formattedMergedStr = _formatMergedList(mergedFullList)
-
-  /* 上传最终的合并列表 */
-  // 1 上传到ipfs
-  let lenOfformattedMergedStr = strLenInHuman(formattedMergedStr)
-  logger.info(`[${type}]:start to upload merged list to ipfs:${lenOfformattedMergedStr}`)
-  startTime = new Date().getTime()
-  let ipfsInfo = await ipfsCliRemote.addByStr(formattedMergedStr, {
-    progress: function (uploadedSize) {
-      uploadedSize = Math.floor(uploadedSize / (1024 * 1024))
-      logger.info(`[${type}]:progress of uploading to ipfs:${uploadedSize}`)
-    }
-  })
-  endTime = new Date().getTime()
-  logger.info(`[${type}]:end to upload merged list to ipfs:${endTime - startTime}ms`)
-  ipfsInfo.name = type + '-merged-' + new Date().getTime() + '.txt'
-
-  // 2 上传到账本
-  if (!latestVersion) {
-    let version = await queryCC('version', [])
-    latestVersion = parseInt(version)
-  }
-
-  ipfsInfo = JSON.stringify(ipfsInfo)
+/**
+ * 确认提交合并
+ **/
+async function commitMerge (type, latestVersion, latestMergeIpfsInfo, bloomFilter) {
   if (type === 'ip') {
     let bloomBuckets = JSON.stringify([].slice.call(bloomFilter.buckets))
-    await invokeCC('uploadMergeList', [ipfsInfo, type, latestVersion + '', bloomBuckets])
+    await invokeCC('uploadMergeList', [latestMergeIpfsInfo, type, latestVersion + '', bloomBuckets])
   } else {
-    await invokeCC('uploadMergeList', [ipfsInfo, type, latestVersion + ''])
+    await invokeCC('uploadMergeList', [latestMergeIpfsInfo, type, latestVersion + ''])
   }
 
   // 链码中投票合并
   await invokeCC('merge', [type, new Date().getTime().toString()])
-  logger.info(`[${type}] new merge list:${ipfsInfo}`)
+  logger.info(`[${type}] new merge list:${latestMergeIpfsInfo}`)
 }
 
 async function getMergedRmList (type) {
@@ -167,54 +135,6 @@ async function _uploadToIpfs (filename, type, dataList) {
   let endTime = new Date().getTime()
   logger.info(`end to upload ${type}-${filename} to ipfs:timestamp:${endTime - startTime}`)
   return uploadedIpfsinfo
-}
-
-/**
- * 获取共识的移除清单
- **/
-async function _getConsensusedRmList (type) {
-  let mergedRmList = await getMergedRmList(type)
-
-  // 获取共识标准
-  let minRmVotesOfConsensus = await _getQuorum()
-
-  let rmSetOfConsensus = new Set()
-
-  for (let record in mergedRmList) {
-    let recordVotes = mergedRmList[record].size
-
-    if (recordVotes >= minRmVotesOfConsensus) {
-      rmSetOfConsensus.add(record)
-    }
-  }
-
-  return rmSetOfConsensus
-}
-
-/**
- * 申诉列表共识所需最小票数为所有成员数的1/3
- */
-async function _getQuorum () {
-  let resp = await agent.post(ADMIN_ADDR + '/peer/peers2').buffer()
-  let peers = JSON.parse(resp.text)
-  let limit = Math.ceil(peers.length / 3)
-
-  return limit
-}
-
-/**
- * 获取合并的全量列表
- **/
-async function _getMergedFullListOfOrgs (type) {
-  let fullListOfArgs = await queryCC('getAllOrgsList', [type])
-
-  logger.info(`[${type}] current orgs full list:${fullListOfArgs}`)
-  // 下载全量列表
-  let fullListFileInfos = await _downloadDataFromIPFS(fullListOfArgs)
-
-  // 合并所有组织的记录的投票
-  let mergedFullList = _groupVoterByRecord(fullListFileInfos)
-  return mergedFullList
 }
 
 /**
@@ -280,84 +200,6 @@ function _groupVoterByRecord (listFileInfos) {
 }
 
 /**
- *  提出没达到票数的记录，和存在于申诉列表中的记录
- **/
-function _getFinnalRecords (mergedFullList, rmSetOfConsensus, type) {
-  let tmpMergedFullList = {}
-  var bloomFilter = new BloomFilter(
-    8 * 256, // number of bits to allocate.
-    16 // number of hash functions.
-  )
-  for (let record in mergedFullList) {
-    if (rmSetOfConsensus.has(record)) continue
-
-    // 剔除未达到指定票数的记录
-    if (type === 'ip' || type === 'default') { // 必须2个以上投票
-      let votes = mergedFullList[record].size
-
-      if (votes < 2) {
-        continue
-      }
-    }
-
-    // 生成ip的布隆过滤器
-    if (type === 'ip') {
-      bloomFilter.add(record)
-    }
-
-    tmpMergedFullList[record] = mergedFullList[record]
-  }
-
-  return {tmpMergedFullList, bloomFilter}
-}
-
-/**
- * 过滤掉媒体ip
- **/
-async function filterPublisherIps(mergedIps) {
-  // eg:"["127.0.0.1\n127.0.0.2",]"
-  let publisterIps = await queryCC('getPublisherIp', [])
-  if (!publisterIps) return mergedIps
-
-  let ips = Object.keys(mergedIps)
-  ips.forEach(function (ip) {
-    if (publisterIps.indexOf(ip) !== -1) { // 为媒体ip
-      delete mergedIps[ip]
-    }
-  })
-  return mergedIps
-}
-
-/**
- * 将投票集合转换为列表
- **/
-function _formatMergedList (mergedFullList) {
-  let formattedStr = ''
-
-  // 记录排序
-  let records = Object.keys(mergedFullList)
-  records.sort()
-  let len = records.length
-
-  for (let i = 0; i < len; i++) {
-    let record = records[i]
-
-    let votesSet = mergedFullList[record]
-
-    // 投票排序
-    let votes = Array.from(votesSet).sort().join(',')
-
-    formattedStr += record + ':' + votes
-
-    if (i !== len - 1) { // 换行
-      formattedStr += '\n'
-    }
-  }
-
-  return formattedStr
-}
-
-/**
  * 计算字符串大小
  **/
 function strLenInHuman(str) {
@@ -385,4 +227,5 @@ exports.uploadBlacklist = uploadBlacklist
 exports.commitBlacklist = commitBlacklist
 
 exports.merge = merge
+exports.commitMerge = commitMerge
 exports.getMergedRmList = getMergedRmList
