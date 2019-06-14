ab68cc838d4b43762bdaded7cbcbad28c6c1d312
kk <kiky.li@rtbasia.com>
2019-06-14 18:55:55 +0800

    修复stage环境

---
+++ build_test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker build -t rtbasia/dif-client-test .
+
+docker tag rtbasia/dif-client-test:latest dockerhub.rtbasia.com/dif/dif-client-test:latest
+
+docker push dockerhub.rtbasia.com/dif/dif-client-test:latest
+
+docker images | grep "^<none>"| awk '{print $3}' | xargs docker rmi -f
--- clear-peer-cli.sh
+++ clear-peer-cli.sh
@@ -3,27 +3,7 @@
 ######
 # 清空 客户端 节点
 ######
-######
-# 倒计时
-######
-function countDown(){
-    local seconds_left=$1
 
-    echo "等待${seconds_left}秒……"
-    while [ $seconds_left -gt 0 ];do
-      echo -n $seconds_left
-      sleep 1
-      seconds_left=$(($seconds_left - 1))
-      echo -ne "\r     \r" #清除本行文字
-    done
-}
-
-echo "清理临时文件————>"
-rm -rf /tmp/dif-kvs/
-
-echo "清理容器————>"
+echo "清理client节点————>"
 docker-compose -f docker-compose-peer.yaml down --volumes
 docker ps -a | grep -v 'CONTAINER' |awk '{print $1}' | xargs -r docker rm -fv
-
-echo "关闭app————>"
-ps -efww |grep -w 'app.js' |grep -v grep |awk '{print $2}' | xargs -r kill -9
